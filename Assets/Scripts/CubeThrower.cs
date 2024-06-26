7c545e1948c9b56a798dd609a9cbb3aaff724b6d
DarkVenusJB <Darkvenus.job@gmail.com>
2024-06-26 19:02:29 +0300

    Get Rotation Angle

--- Assets/Scripts/CubeThrower.cs
+++ Assets/Scripts/CubeThrower.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -7,22 +6,33 @@ public class CubeThrower : MonoBehaviour
     [SerializeField] private int targetValue;
     [SerializeField] private float dropForce;
 
+    [SerializeField] private Transform[] Edge;
+    
+    
+
     private Rigidbody rigidbody;
 
     private void Awake()
     {
+        
+        Debug.Log(Vector3.Angle(transform.up, Edge[5].position));
         rigidbody = GetComponent<Rigidbody>();
     }
 
     private void Start()
     {
-        Drop();
+       // Drop();
     }
 
     private void Drop()
     {
         GetRandomRotation();
         rigidbody.AddForce(Vector3.forward*dropForce,ForceMode.Impulse);
+    }
+
+    private void Update()
+    {
+       
     }
 
     private void GetRandomRotation()
